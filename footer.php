e76eb8c9d6847434894f6da3302259a8f5ed2eb0
Yoshie <yoshiemuranaka@gmail.com>
2016-03-16 19:26:38 -0400

    footer logo working

--- footer.php
+++ footer.php
@@ -2,9 +2,9 @@
 		</div><!-- #content -->
 
 		<footer class="footer">
-<!--  
-		<?php get_template_part('/images/inline', 'logoWatermark.svg') ?>
--->
+  
+			<?php get_template_part('/images/footer', 'logo.svg'); ?>
+
 			<p class="copyright">&copy; 2016 Your Best Prep</p>
 			<a class="link-style">Ariel@yourbestprep.com</a>
 			<?php wp_nav_menu( array('container_class' => 'footer-menu', 'menu_id' => 'secondary-menu', 'theme_location' => 'secondary' ) ); ?>
--- page.php
+++ page.php
@@ -5,12 +5,10 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-
-<!-- GETTING PAGE CONTENT -->
-	<?php if (have_posts()) : while (have_posts()) : the_post();?>
-	<?php the_content(); ?>
-	<?php endwhile; endif; ?>
-
+			<!-- GETTING PAGE CONTENT -->
+			<?php if (have_posts()) : while (have_posts()) : the_post();?>
+			<?php the_content(); ?>
+			<?php endwhile; endif; ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
