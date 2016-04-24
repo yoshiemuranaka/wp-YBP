adc74a52a96378d714703ee479583532b538154b
Yoshie <yoshiemuranaka@gmail.com>
2016-04-23 23:51:40 -0400

    adding services icon

--- template-parts/page-services.php
+++ template-parts/page-services.php
@@ -19,8 +19,29 @@ get_header(); ?>
 					</h1>
 				</div>
 			</div>
+			<div class="services-icons">
+				<div class="grid">
+					<div class="col one-quarter">
+						<img class="icon" src="<?php bloginfo('stylesheet_directory'); ?>/images/services-icon--test-prep.png" alt="Your Best Prep Services Standardized Test Preparation">
+						<h3>Standardized Test Preparation</h3>	
+					</div>
+					<div class="col one-quarter">
+						<img class="icon" src="<?php bloginfo('stylesheet_directory'); ?>/images/services-icon--academic-subjects.png" alt="Your Best Prep Services Academic Subjects">
+						<h3>Academic Subjects</h3>	
+					</div>
+					<div class="col one-quarter">
+						<img class="icon" src="<?php bloginfo('stylesheet_directory'); ?>/images/services-icon--college-essay.png" alt="Your Best Prep Services College Essay Coaching">
+						<h3>College Essay Coaching</h3>	
+					</div>
+					<div class="col one-quarter">
+						<img class="icon" src="<?php bloginfo('stylesheet_directory'); ?>/images/services-icon--differentiated-instruction.png" alt="Your Best Prep Services Differentiated Instruction for Students with Disabilities">
+						<h3>Differentiated Instruction</h3>	
+					</div>
+				</div>
+			</div>
 			<?php the_content(); ?>
 			<?php endwhile; endif; ?>
+
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
